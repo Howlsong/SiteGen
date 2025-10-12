70c0f5e7f2f48c12f26360fb6423543717ab2133
Howlsong <howl@DarkForest.lan>
2025-10-12 07:18:22 -0500

    Wrote tests

---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_neq_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, url="http://guthib.com")
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_neq_type(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_neq_text(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a different text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.BOLD, url="http://guthib.com")
+        expected_repr = "TextNode(This is a text node, TextType.BOLD, http://guthib.com)"
+        self.assertEqual(repr(node), expected_repr)
+
+
+if __name__ == "__main__":
+    unittest.main()
