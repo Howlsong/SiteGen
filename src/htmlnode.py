b71a47709d41129e391aa7e4b777b7c82fa34e08
Howlsong <howl@DarkForest.lan>
2025-10-16 19:21:44 -0500

    Parent Nodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -31,3 +31,16 @@ class LeafNode(HTMLNode):
         if self.tag == None:
             return self.value
         return f"<{self.tag}>{self.value}</{self.tag}>"
+    
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+    
+    def to_html(self):
+        if self.tag == None:
+            raise ValueError("ParentNode must have a tag")
+        if self.children == None:
+            raise ValueError("ParentNode must have children")
+        children_html = "".join([child.to_html() for child in self.children])
+        props_html = self.props_to_html()
+        return f"<{self.tag}{props_html}>{children_html}</{self.tag}>"
